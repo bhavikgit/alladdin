24943f8e8a0981f57ed8ed3245bb5a08b8c0527c
bhavikgit <bhavik199366@yahoo.in>
2018-02-22 12:19:25 +0530

    city uuid function done

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -6,8 +6,7 @@ module FiltersHelper
   INDEX_NAME = "polygon_index"
   INDEX_TYPE = "polygons"
 
-
-
+  ## not used anymore
   def polygons_filter(text)
     localities_data, cities_data = [], []
     CSV.foreach("tmp/polygons_data_dump.csv", headers: true) do |row|
@@ -19,10 +18,10 @@ module FiltersHelper
     return localities_data.uniq, cities_data.uniq
   end
 
-  def get_relevant_polygons(text)
+  def get_relevant_polygons(words)
     localities_data, cities_data = [], []
 
-    query = get_es_query(text)
+    query = get_es_query(words)
     poly_results = get_poly_results_from_es(query)
     
     hits = poly_results["hits"]["hits"]
@@ -38,7 +37,8 @@ module FiltersHelper
     end
   end
 
-  def get_es_query(text)
+  def get_es_query(words)
+    text = words.join(" ")
     {
       "query" => {
         "match" => {
@@ -54,6 +54,13 @@ module FiltersHelper
   def get_poly_results_from_es(query)
     client = ExternalApiHelper.get_client
     return ExternalApiHelper.get_results_from_es(client, INDEX_NAME, INDEX_TYPE, query)
+  end
+
+  def get_city_uuid(words)
+    localities_data, cities_data = get_relevant_polygons(words)
+    locality_city_uuids = localities_data.map {|locality| locality["city_uuid"]}
+    city_uuids = cities_data.map {|city| city["uuid"]}
+    locality_city_uuids.max_by { |i| locality_city_uuids.count(i) } || city_uuids.max_by { |i| city_uuids.count(i) } 
   end
 
   def apartment_type_filter(apartment_type)
